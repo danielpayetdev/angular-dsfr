36f640944d980a8d1ee47a620aff05811db8c461
Daniel Payet <danieldu974@gmail.com>
2022-12-28 18:53:34 +0100

    enable theming (light,dark and system mode)

--- demo/src/main.ts
+++ demo/src/main.ts
@@ -1,4 +1,16 @@
+import { APP_INITIALIZER } from "@angular/core";
 import { bootstrapApplication } from "@angular/platform-browser";
+import { ThemeService } from "angular-dsfr";
 import { AppComponent } from "./app/app.component";
 
-bootstrapApplication(AppComponent);
+bootstrapApplication(AppComponent, {
+    providers: [
+        {
+            provide: APP_INITIALIZER,
+            useFactory: (themeService: ThemeService) => () =>
+                themeService.setTheme(themeService.getTheme()),
+            deps: [ThemeService],
+            multi: true
+        }
+    ]
+});
